95dd12f4ac5aa9d5e3d662445354563740768f61
Swastik <shuklaswastik977@gmail.com>
2026-08-24 09:35:46 +0530

    Change break statement to exit outer loop

--- two_sum.java
+++ two_sum.java
@@ -1,13 +1,13 @@
 public class two_sum {
     public static void main(String[] args) {
-        int[] arr= {4,7,3,2,9,6};
+        int[] arr= {4,7,3,2,9};
         int target= 13;
 
         outer : for(int i=0;i<arr.length;i++){
             inner : for(int j=i+1;j<arr.length;j++){
                 if(arr[i]+arr[j]==target){
                     System.out.println(arr[i]+" + "+arr[j]+" = "+target);
-                    break inner;
+                    break outer;
                 }
             }
         }
