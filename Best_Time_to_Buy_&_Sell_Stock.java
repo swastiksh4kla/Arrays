e68f785f608dc45f7e0b96eb4cc9a367abd80238
Swastik <shuklaswastik977@gmail.com>
2026-08-26 12:41:21 +0530

    Best_Time_to_Buy_&_Sell_Stock.java uploaded
    Implement a solution to find the maximum profit from stock prices.

---
+++ Best_Time_to_Buy_&_Sell_Stock.java
@@ -0,0 +1,18 @@
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int sell = 0;
+
+        for(int i = 1;i < prices.length;i++){
+            if(prices[i] < buy){
+                buy = prices[i];
+            }
+            else if(prices[i] - buy > sell){
+                sell = prices[i] - buy;
+            }
+        }
+        return sell;
+    }
+}
