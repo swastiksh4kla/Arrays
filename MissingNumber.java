a61bf8996ece9b617769aba574c9c603a2ac1c64
Swastik <shuklaswastik977@gmail.com>
2026-08-31 11:29:20 +0530

    Implement missing number solution using XOR
    Added a new method using XOR operator to find the missing number.

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,5 +1,5 @@
-//Time Complexity: O(n)
-//Space Complexity: O(1)
+//Using Gauss's Formula (risk of integer overflow)
+//Time Complexity: O(n)    &    Space Complexity: O(1)
 public class MissingNumber {
     public static void main(String[] args) {
         //using long variable to avoid number overflow bugs
@@ -11,5 +11,20 @@ public class MissingNumber {
             arrSum+=arr[i];
         }
         System.out.println(sum-arrSum);
+    }
+}
+
+//Using XOR(^) Operator to avoid overflow
+//Time Complexity: O(n)    &    Space Complexity: O(1)
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor= 0;
+        for(int i=0; i<=nums.length; i++){
+            xor ^= i;
+        }
+        for(int x: nums){
+            xor ^= x;
+        }
+        return xor;
     }
 }
