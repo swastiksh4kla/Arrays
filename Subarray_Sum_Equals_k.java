89e9cac84a37c585985f4875328500a048dea873
Swastik <shuklaswastik977@gmail.com>
2026-09-07 13:50:11 +0530

    Subarray_Sum_Equals_k uploaded

---
+++ Subarray_Sum_Equals_k.java
@@ -0,0 +1,19 @@
+//Time & Space complexity: O(n)
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        int sum = 0;
+        map.put(0,1);
+
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            int req = sum - k;
+            if(map.containsKey(req)){
+                count += map.get(req);
+            }
+            map.put(sum, map.getOrDefault(sum, 0)+1);
+        }
+        return count;
+    }
+}
