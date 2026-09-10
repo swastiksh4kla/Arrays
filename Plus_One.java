3893e70d995467eb49fc9b0e576b3c7025c53de1
Swastik <shuklaswastik977@gmail.com>
2026-09-10 09:50:21 +0530

    Add plusOne method to increment digits array

---
+++ Plus_One.java
@@ -0,0 +1,16 @@
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1; i>=0; i--){
+            if(digits[i]+1!=10){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigits = new int[digits.length+1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+}
